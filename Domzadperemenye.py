c82ee0b92b2a1a23e0f6f8e9a81bc61261bb757f
Satasert <bazulko606199@gmail.com>
2024-09-07 12:43:21 +0500

    Add files via upload

---
+++ Domzadperemenye.py
@@ -0,0 +1,5 @@
+Homework_count = 12
+Hours_count = 1.5
+Name_kurs = "Pyton"
+Time_per_task= Hours_count/Homework_count
+print("Курс: ",Name_kurs,","" всего задач:" ,Homework_count,","" затрачено часов:",Hours_count,","" среднее время выполнения ", Time_per_task, " часа.", sep="" )
