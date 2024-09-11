6fedced6e8140ecaaad992b6bdf84a55415ad2b1
Satasert <bazulko606199@gmail.com>
2024-09-11 17:43:54 +0500

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,14 @@
+#Задача "Однокоренные":
+def single_root_words(root_word, *other_words): #Обявляем фунцию
+    root_word_lower = root_word.lower()
+    same_words = [] #Создаем пустой список
+    for word in other_words: #Перебираем слова
+        word_lower = word.lower()
+        if root_word_lower in word_lower or word_lower in root_word_lower:
+            same_words.append(word)
+    return same_words #Возвращаем список
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
